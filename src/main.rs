b565bda1b15e3cec338fb122e16b723316f6cc86
kawadumax <kazmax7@gmail.com>
2021-02-18 10:53:32 +0900

    pdfフォルダ作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use printpdf::*;
+use std::fs::File;
+use std::io::BufWriter;
+
 fn main() {
-    println!("Hello, world!");
+
+    
+    let (doc, page1, layer1) = PdfDocument::new("PDF_Document_title", Mm(247.0), Mm(210.0), "Layer 1");
+    // let (page2, layer1) = doc.add_page(Mm(10.0), Mm(250.0),"Page 2, Layer 1");
+    
+    doc.save(&mut BufWriter::new(File::create("pdf/test_working.pdf").unwrap())).unwrap();
 }
