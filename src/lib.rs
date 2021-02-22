3c7280ccc99ce165d7f6efe8dabcf35ad18df450
kawadumax <kazmax7@gmail.com>
2021-02-22 10:07:09 +0900

    util追加（何もない）

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod data_generator;
 pub mod mode;
 pub mod task_builder;
+// pub mod util;
 
 pub use data_generator::*;
 pub use mode::Mode;
 pub use task_builder::TaskBuilder;
+// pub use util::nloop;
---
+++ src/util.rs
@@ -0,0 +1,6 @@
+//Rust力が足りなくて実装できない
+// pub fn nloop(n: usize, f: Fn()) {
+//     for i in 0..n {
+//         f();
+//     }
+// }
